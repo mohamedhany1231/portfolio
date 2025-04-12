e23c5b7d5029014f345b2d712eb56fde8bdfd681
mohamedhany1231 <mohamedhany61@gmail.com>
2025-04-12 22:27:35 +0200

    remove unsused imports from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Hero from "./pages/Hero.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
